a84c35c574c3163b704012e44aa9c1e59662a6bc
Abraao Brito <abraaobritof10@gmail.com>
2021-01-24 15:02:26 -0400

    commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { HttpClientModule } from '@angular/common/http';
+
 import { AppComponent } from './app.component';
+import { AlunosComponent } from './alunos/alunos.component';
+import { AlunosService } from './alunos.service';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [AppComponent, AlunosComponent],
+  imports: [BrowserModule, HttpClientModule],
+  providers: [HttpClientModule, AlunosService],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
